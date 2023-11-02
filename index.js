4094946900047b37e040cac517df437d069ef87f
amgi <maginci@gmail.com>
2023-11-02 14:20:17 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const inputField = document.getElementById('input-el')
+const convertBtn = document.getElementById('convert-btn')
+const convertLength = document.getElementById('conv-length')
+const convertVolume = document.getElementById('conv-volume')
+const convertMass = document.getElementById('conv-mass')
+    
+convertBtn.addEventListener('click', function() {
+       
+        let inputNum = inputField.value
+       
+        let metersToFeet = (inputNum * 3.28084).toFixed(3)
+        let feetToMeters = (inputNum / 3.28084).toFixed(3)
+        convertLength.textContent = `${inputNum} meters = ${metersToFeet} feet | ${inputNum} feet = ${feetToMeters} meters`
+        let litersToGalons = (inputNum * 0.264172).toFixed(3)
+        let galonsToLiters = (inputNum / 0.264172).toFixed(3)
+        convertVolume.textContent = `${inputNum} liters = ${litersToGalons} gallons | ${inputNum} gallons = ${galonsToLiters} liters` 
+        let kilosToPounds = (inputNum * 2.20462).toFixed(3)
+        let poundsToKilos = (inputNum / 2.20462).toFixed(3)
+        convertMass.textContent = `${inputNum} kilos = ${kilosToPounds} pounds | ${inputNum} pounds = ${poundsToKilos} kilos`
+    })
+    
